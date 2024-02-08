0c85dbf26654ce54f313f93a2f59d5b7376ed743
Rohit <rohit.verma@itsrizzoli.it>
2024-02-08 23:34:27 +0100

    Javadoc updated successfully.

---
+++ src/JavaDoc/package-search-index.js
@@ -0,0 +1,1 @@
+packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"l":"store.client"},{"l":"store.interfaces"},{"l":"store.server"}];updateSearchResults();
---
+++ src/JavaDoc/type-search-index.js
@@ -0,0 +1,1 @@
+typeSearchIndex = [{"l":"All Classes and Interfaces","u":"allclasses-index.html"},{"p":"store.server","l":"Animal"},{"p":"store.interfaces","l":"Animal_Preferences"},{"p":"store.interfaces","l":"Description"},{"p":"store.server","l":"FoodAndAccessories"},{"p":"store.server","l":"Pet"},{"p":"store.client","l":"User"},{"p":"store.client","l":"UserData"}];updateSearchResults();
